5e4cc8466a23e951f7931c0d61e580dc5a0c575d
Job Karani <jobkarani084j@gmail.com>
2023-03-01 07:45:27 +0300

    feat: create operations

--- app/views.py
+++ app/views.py
@@ -1,34 +1,29 @@
-from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
-from django.db.models import Q
-# from simple_mail.mail import send_mail
-
 from app.models import *
 from .serializer import *
-from .pagination import *
-from rest_framework import generics
-from rest_framework.pagination import PageNumberPagination
 
 
 # Create your views here
 
+# CREATE operations
+@api_view(['POST'])
+def contact_create(request):
+    if request.method == "GET":
+        serializer = ContactSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+#  READ operations
 
 @api_view(['GET',])
 def contacts(request):
     if request.method == "GET":
         contacts = Contact.objects.all()
-
-        # Set up pagination
-        paginator = PageNumberPagination()
-        paginator.page_size = 300
-        result_page = paginator.paginate_queryset(contacts, request)
-
-        # Serialize the result page
-        serializer = ContactSerializer(result_page, many=True)
+        serializer = ContactSerializer(contacts, many=True)
         return Response(serializer.data)
 
 @api_view(['GET'])
@@ -37,3 +32,4 @@ def contactDetails(request, contact_id):
         contact= Contact.objects.filter(id = contact_id)
         serializer = ContactSerializer(contact, many=True)
         return Response(serializer.data)
+
