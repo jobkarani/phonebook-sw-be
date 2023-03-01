eb9e2667b57f35279792ef4bd8203892fbd4b8b8
Job Karani <jobkarani084j@gmail.com>
2023-03-01 08:57:55 +0300

    fit: migrations

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class Contact(models.Model):
     lastName = models.CharField(max_length=200, null=True)
     email = models.EmailField(max_length=256, unique=True)
     phoneNumber = models.IntegerField(max_length=200, null=True)
-    profilePicture = ImageField( manual_crop="")
+    profilePicture = ImageField( manual_crop="", null=True)
     location = models.CharField(max_length=200, null=True)
 
     class Meta:
